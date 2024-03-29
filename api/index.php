7eb17bca96af8fa979c2a90c7d5df794a937f310
Fabricionunesm <fnunes87407385@gmail.com>
2024-03-29 18:56:41 -0300

    atualização2

--- api/index.php
+++ api/index.php
@@ -1,9 +1,11 @@
 <?php
+$teste = php_info();
     echo '
      <html>
         <head>
         </head>
         <body>
+        '.$teste.'
             <h1>Apenas testando</h1>
         </body>
     </html>';
